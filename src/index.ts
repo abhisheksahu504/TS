1a99cedae54bb6da985cd02f4cf0aa86e2a3c522
Abhishek Sahu <abhisheksahu504@gmail.com>
2024-10-19 19:48:53 +0530

    used query selectors to target elements

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const getUsername = document.querySelector("#user") as HTMLInputElement;
+const formSubmit = document.querySelector(".form") as HTMLFormElement;
+const main_container = document.querySelector(".main-container") as HTMLElement;
